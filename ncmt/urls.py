a39803f9db0db4b40c0be086943b0999752f52f1
Tej749 <jargha261975@gmail.com>
2024-11-27 22:29:13 +0545

    add master page

--- ncmt/urls.py
+++ ncmt/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("",views.master, name="master"),
+    path("index",views.index, name="index"),
     path("bca", include("bca.urls")),
     path("org", include("org.urls")),
     path("csit", include("csit.urls")),
